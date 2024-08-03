79597b446a8ff324e5eeebeb37b03819d9616294
Форов Александр <5h9t5@list.ru>
2024-08-03 18:07:30 +0300

    feat(main): added typing

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import uvicorn
 
 from fastapi import FastAPI
+from typing import Optional
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def read_root():
-    return {'Hello': 'FastAPI'}
+@app.get('/me')
+def hello_author():
+    return {'Hello': 'author'}
+
+
+@app.get('/{name}')
+def greetings(
+        name: str,
+        surname: str,
+        age: Optional[int] = None,
+        is_staff: bool = False
+) -> dict[str, str]:
+    result = ' '.join([name, surname])
+    result = result.title()
+    if age is not None:
+        result += ', ' + str(age)
+    if is_staff:
+        result += ', сотрудник'
+    return {'Hello': result}
+
+
+@app.get('/multiplication')
+def multiplication(
+    length: int,
+    width: int,
+    depth: Optional[int] = None
+) -> int:
+    result = length * width
+    if depth is not None:
+        result *= depth
+    return result
 
 
 if __name__ == '__main__':
